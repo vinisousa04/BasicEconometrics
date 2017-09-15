60000ee20c53d9c1c3042f5b4cd83f5f1c33ad91
Vinicius <sousavini04@hotmail.com>
2017-09-15 18:26:58 -0300

    Testing adding a data set to the package

---
+++ R/data.R
@@ -0,0 +1,12 @@
+#' Times of a atheletes before and after a training camp
+#'
+#' a data set containing the times before and after of a group
+#' of athletes before and after receiving a training
+#'
+#' @format A data frame with 12 rows and 3 columns:
+#' \describe{
+#' \item{AthleteID}{Athlete identification, by numbers}
+#' \item{Time1}{Time before the training, in minutes}
+#' \item{Time2}{Time after the training, in minutes}
+#' }
+"data"
